44c1f7da733007e2fb20559a2d83a9c197bca7ee
Fercho1118 <rue23748@uvg.edu.gt>
2024-09-08 20:27:18 -0600

    Logo en login
    Se agregó el logo en el login

--- LAB8/app/src/main/java/com/uvg/rueda/lab08/login/logScreen.kt
+++ LAB8/app/src/main/java/com/uvg/rueda/lab08/login/logScreen.kt
@@ -1,5 +1,6 @@
 package com.uvg.rueda.lab08.login
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.material3.Button
@@ -9,8 +10,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import com.uvg.rueda.lab08.R
 
 @Composable
 fun LoginScreen(onNavigateToCharacters: () -> Unit) {
@@ -23,6 +27,15 @@ fun LoginScreen(onNavigateToCharacters: () -> Unit) {
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
+            Image(
+                painter = painterResource(id = R.drawable.logorickandmorty),
+                contentDescription = "Rick and Morty Logo",
+                modifier = Modifier
+                    .fillMaxWidth(0.8f)
+                    .aspectRatio(2f)
+                    .padding(bottom = 24.dp),
+                contentScale = ContentScale.Fit,
+            )
             Spacer(modifier = Modifier.height(200.dp))
             Button(onClick = onNavigateToCharacters) {
                 Text(text = "Entrar")
