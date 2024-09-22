859d3607578e5090773e6868fd8559ea02cbc15f
Fercho1118 <rue23748@uvg.edu.gt>
2024-09-21 23:22:45 -0600

    Pantalla de perfil
    Se programó la pantalla donde se verá el perfil del usuario y tiene un botón para cerrar sesión.

--- LAB8/app/src/main/java/com/uvg/rueda/lab08/profile/profileScreen.kt
+++ LAB8/app/src/main/java/com/uvg/rueda/lab08/profile/profileScreen.kt
@@ -1,2 +1,64 @@
 package com.uvg.rueda.lab08.profile
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun ProfileScreen(onLogout: () -> Unit) {
+    Scaffold { paddingValues ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(paddingValues)
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Box(
+                modifier = Modifier
+                    .size(128.dp)
+                    .clip(CircleShape)
+                    .background(MaterialTheme.colorScheme.primary.copy(alpha = 0.2f)),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = "Profile Icon",
+                    modifier = Modifier.size(64.dp),
+                    tint = MaterialTheme.colorScheme.onPrimary
+                )
+            }
+            Spacer(modifier = Modifier.height(16.dp))
+            Text(
+                text = "Nombre: Fernando Rueda",
+                color = MaterialTheme.colorScheme.onSurface
+            )
+            Text(
+                text = "Carné: 23748",
+                color = MaterialTheme.colorScheme.onSurface 
+            )
+            Spacer(modifier = Modifier.height(32.dp))
+            // Botón para cerrar sesión
+            Button(onClick = onLogout) {
+                Text(text = "Cerrar sesión")
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewProfileScreen() {
+    ProfileScreen(onLogout = {})
+}
